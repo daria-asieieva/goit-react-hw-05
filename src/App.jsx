fb6588f7276179a14abefc78768a74ae8a94b2f5
Daria <dariaasieieva@gmail.com>
2025-01-12 22:48:34 +0100

    1

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,15 @@ import { Puff } from "react-loader-spinner";
 
 import css from "./App.module.css";
 
-import HomePage from "./pages/HomePage/HomePage";
-import MoviesPage from "./pages/MoviesPage/MoviesPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
-import MovieCast from "./components/MovieCast/MovieCast";
-import MovieReviews from "./components/MovieReviews/MovieReviews";
 
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 const MovieDetailsPage = lazy(() =>
   import("./pages/MovieDetailsPage/MovieDetailsPage")
 );
 const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
-const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
-);
+const MovieReviews = lazy(() => import("./components/MovieReviews/MovieReviews"));
 
 function App() {
   const linkActiveStyle = ({ isActive }) => {
